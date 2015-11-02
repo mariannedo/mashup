09d81bcd2a60811de407bcf27ef12a7c3273f0b2
mariannedo <mariannepdo@gmail.com>
2015-11-01 20:24:32 -0500

    setting up ajax calls to both apis, linking

---
+++ main.js
@@ -0,0 +1,87 @@
+var ShowShows = { 
+    init: function() { 
+        $.ajax({
+            url: "http://api.bandsintown.com/events/search.json?location=New+York,NY&radius=10&page=1&per_page=5&app_id=Marianne_JS_APP",
+            dataType: "jsonp", 
+
+        }).done(function(events) {
+            ShowShows.mapCenter = ShowShows.calcMapCenter(events); 
+            ShowShows.initMap(); 
+
+            events.forEach(function(event) { 
+                ShowShows.createHtml(event);  
+            }); 
+
+        });
+    }, 
+
+    createHtml: function(event) { 
+        var htmlBlock = '<div class="map-block">'; 
+
+        for(var i = 0; i < event.artists.length; i++) { 
+            htmlBlock += event.artists[i]['name'] + '<br/>'; 
+        }
+
+        htmlBlock += event.venue.name + '<br/>'; 
+
+        htmlBlock += new Date(event.datetime); 
+
+        htmlBlock += '<a href="' + event.url + ' target="_blank">more details</a>';
+
+        htmlBlock += '</div>'; 
+
+        ShowShows.addMapMarker(htmlBlock, event.venue); 
+    }, 
+
+    addMapMarker: function(htmlBlock, venue) { 
+        var newLoc = {
+            lat: venue.latitude, 
+            lng: venue.longitude
+        };
+
+        var infowindow = new google.maps.InfoWindow({
+            content: htmlBlock
+        });
+
+        var marker = new google.maps.Marker({
+            position: newLoc,
+            map: ShowShows.map,
+            title: venue.name
+        });
+
+        marker.addListener('click', function() {
+            infowindow.open(ShowShows.map, marker);
+        });
+    },
+
+    calcMapCenter: function(events) { 
+        var venueLat  = 0, 
+            venueLong = 0,
+            locationObj = {}; 
+
+        events.forEach(function(event) { 
+            venueLat += event.venue.latitude; 
+            venueLong += event.venue.longitude; 
+        }); 
+
+        venueLat = venueLat/events.length; 
+        venueLong = venueLong/events.length; 
+
+        locationObj.lat = venueLat; 
+        locationObj.lng = venueLong; 
+
+        return locationObj; 
+    }, 
+
+    initMap: function() { 
+        ShowShows.map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 12,
+            center: ShowShows.mapCenter
+        });
+    }
+}
+
+
+$(document).ready(function() { 
+    ShowShows.init(); 
+}); 
